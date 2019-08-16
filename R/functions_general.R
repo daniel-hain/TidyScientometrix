d1e46b3f6d631a683335d15b24e886d23032d58d
Daniel S. Hain <38432063+daniel-hain@users.noreply.github.com>
2019-08-16 13:50:08 +0200

    Update functions_general.R

--- R/functions_general.R
+++ R/functions_general.R
@@ -1,14 +1,3 @@
-
-##################################################################
-##	BEGIN: norm_01
-##################################################################
-
-norm_01 <- function(x, ...){
-  x_min = min(x, ...)
-  x_max = max(x, ...)
-  x <- (x - x_min) / (x_max - x_min)
-}
-
 ##################################################################
 ##	BEGIN: Jaccard Weight
 ##################################################################
@@ -16,10 +5,10 @@ norm_01 <- function(x, ...){
 weight_jaccard <- function(x, i, j, w) {
   y <- x %>% select_(i, j, w)
   colnames(y) <- c("i", "j", "w")
-  
+
   y %<>% group_by(i) %>% mutate(w.i = sum(w)) %>% ungroup() %>%
     group_by(j) %>% mutate(w.j = sum(w)) %>% ungroup() %>%
-    mutate(weight_jac = w / (w.i + w.j - w)) %>% 
+    mutate(weight_jac = w / (w.i + w.j - w)) %>%
     select(weight_jac) %>% pull()
   return(y) # TODO: Find a solution for the case when you have adittional groupings, eg. year
-}  
+}
